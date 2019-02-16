e5945756e33a6b803a2ac3fc47df0bc99f88b3fa
Rebecca <rebeccagolm32@gmail.com>
2019-02-16 11:32:04 -0500

    initial files

---
+++ app/src/main/java/com/example/thisisourapponepointo/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.thisisourapponepointo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.sing_in_window);
+    }
+}
