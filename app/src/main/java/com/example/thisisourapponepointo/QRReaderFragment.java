4396465cb566ec3ade6021ddce1340d6f77c317c
Rebecca <rebeccagolm32@gmail.com>
2019-02-16 16:55:16 -0500

    error fix

--- app/src/main/java/com/example/thisisourapponepointo/QRReaderFragment.java
+++ app/src/main/java/com/example/thisisourapponepointo/QRReaderFragment.java
@@ -3,6 +3,9 @@ package com.example.thisisourapponepointo;
 import android.content.Context;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.TextView;
 import com.google.android.gms.vision.barcode.BarcodeDetector;
 
@@ -10,13 +13,13 @@ import com.google.android.gms.vision.barcode.BarcodeDetector;
 public class QRReaderFragment extends Fragment {
     private TextView barcodeValue;
     Context context;
-    protected void onCreate(Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.qrcode_reader_layout, container, false);
         super.onCreate(savedInstanceState);
         context = getContext();
-        setContentView(R.layout.qrcode_reader_layout);
-        barcodeValue = (TextView)findViewById(R.id.display_scanned_info);
-        findViewById(R.id.capture_qrcode).setOnClickListener(context);
+        barcodeValue = (TextView)rootView.findViewById(R.id.display_scanned_info);
+        //rootView.findViewById(R.id.capture_qrcode).setOnClickListener();
         //BarcodeDetector barcodeDetector = new BarcodeDetector.Builder(context).build();
-
+        return rootView;
     }
 }
